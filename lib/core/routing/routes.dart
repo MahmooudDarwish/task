afce5b99aaa318ef6c7ee47d392ad5d40d05f492
Mahmoud <mahmoudsaaddarwish1@gmail.com>
2023-11-01 19:56:12 +0200

    file structure for the new task

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,11 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:task/core/routing/route_names.dart';
+import 'package:task/core/routing/route_paths.dart';
 import 'package:task/features/dashboard/presentation/screens/dashboard_screen.dart';
 
 class Routes {
   static List<RouteBase> routes = <RouteBase>[
     GoRoute(
-      name: RouteNames.keyLogin,
-      path: RoutePath.loginRoutePath,
+      name: RouteNames.dashboard,
+      path: RoutePath.initialRoutePath,
       builder: (context, state) => const DashBoardScreen(),
     ),
   ];
--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:task/core/routing/routes.dart';
 import 'package:task/core/services/dio_helper.dart';
 
 final sl = GetIt.instance;
@@ -7,22 +8,17 @@ final sl = GetIt.instance;
 class ServicesLocator {
   void init() {
     ///Go router
-    sl.registerLazySingleton<GoRouter>(
-            () => GoRouter(routes: AppConstance.routes));
+    sl.registerLazySingleton<GoRouter>(() => GoRouter(routes: Routes.routes));
 
     ///Dio helper
     sl.registerLazySingleton<DioHelper>(() => DioHelper());
 
     /// DATA SOURCE
 
-
     /// Repository
-
 
     /// Use Cases
 
-
     /// Bloc
-
   }
 }
