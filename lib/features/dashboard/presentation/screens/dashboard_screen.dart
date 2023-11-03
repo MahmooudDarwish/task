2f33766ba754f0bab86d5930a1b034689b174ed4
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-11-03 02:20:43 +0200

    .

--- lib/features/dashboard/presentation/screens/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screens/dashboard_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:task/core/services/services_locator.dart';
-import 'package:task/features/dashboard/presentation/controller/user_cubit.dart';
+
 import 'package:task/features/dashboard/presentation/screens/utils/dashboard_strings.dart';
 
 class DashBoardScreen extends StatelessWidget {
@@ -9,14 +7,10 @@ class DashBoardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      lazy: false,
-      create: (context) => sl<UserCubit>()..getUserData(),
-      child: Scaffold(
-        drawer: const Drawer(backgroundColor: Colors.black,),
-        appBar: AppBar(
-          title: const Text(DashBoardString.appBarHeader),
-        ),
+    return Scaffold(
+      drawer: const Drawer(backgroundColor: Colors.black,),
+      appBar: AppBar(
+        title: const Text(DashBoardString.appBarHeader),
       ),
     );
   }
