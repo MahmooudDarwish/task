eb19ba41bc9c3418df35a1dfad6757d44164bc33
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-11-03 20:05:08 +0200

    welcome user

--- lib/features/dashboard/domain_layer/repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:task/core/error/failure.dart';
-import 'package:task/features/dashboard/domain_layer/entity.dart';
-
-abstract class GetUserBaseRepo{
-  Future<Either<ServerFailure , List<UserDataEntity>>> getUserData();
-}
