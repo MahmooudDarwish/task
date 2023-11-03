5147f1a553ee358ad23f027e5cdaeb111899aae0
Mohamed Ahmed <i.mohamedahhmed@gmail.com>
2023-11-03 20:04:28 +0200

    welcome user

--- lib/features/dashboard/presentation/controller/user_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'user_cubit.dart';
-
-abstract class UserState {}
-
-class UserInitial extends UserState {}
-class GetUserLoading extends UserState {}
-class GetUserSuccess extends UserState {}
-class GetUserError extends UserState {}
