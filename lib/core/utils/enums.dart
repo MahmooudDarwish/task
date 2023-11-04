c400387d792fd93ffbf207ac7f9fe945df57eaf6
MahmooudDarwish <147933220+MahmooudDarwish@users.noreply.github.com>
2023-11-04 07:15:14 +0200

    Delete lib directory
    change project

--- lib/core/utils/enums.dart
+++
@@ -1,9 +0,0 @@
-
-enum TabBarItemType { users, services, orders }
-
-enum RequestState {
-  loading,
-  loaded,
-  error,
-}
-
--- lib/features/home/domain/repo/base_get_users_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:task/core/error/failure.dart';
-import 'package:task/features/home/domain/entities/user.dart';
-
-abstract class BaseGetUsersRepo {
-  Future<Either<Failure, List<User>>> getUsers();
-}
